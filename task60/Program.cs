c7e85907baca86259b363151583e39fbb2f4fc0b
Yulia <yulia1201556@gmail.com>
2022-09-06 08:50:04 +0300

    Трёхмерный массив из неповторяющихся двузначных чисел

--- task60/Program.cs
+++ task60/Program.cs
@@ -6,71 +6,88 @@
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-
-int rasm1 = InputInt("Введите размер 1: ");
-int rasm2 = InputInt("Введите размер 2: ");
-int rasm3 = InputInt("Введите размер 3: ");
 
 
-if (rasm1 * rasm2 * rasm3 > countNums)
+int[,,] CreateMatrix(int row, int col, int hgt)
 {
-    Console.Write("Массив слишком большой");
-    return;
+    int[,,] matrix = new int[row, col, hgt];
+    int w = 9;
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    if (w < 100)
+                    {
+                        matrix[i, j, k] = w + 1;
+                        w++;
+
+                    }
+                }
+            }
+        }
+    }
+    return matrix;
 }
 
-int[,,] resultNums = Create3DMassive(rasm1, rasm2, rasm3);
 
-for (int i = 0; i < resultNums.GetLength(0); i++)
+void CreateArrayRnd(int[,,] matrix)
 {
-    for (int j = 0; j < resultNums.GetLength(1); j++)
+    int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+    int number;
+    Random rnd = new Random();
+    for (int i = 0; i < temp.GetLength(0); i++)
     {
-        for (int k = 0; k < resultNums.GetLength(2); k++)
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
         {
-            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = rnd.Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+
+                Console.Write($"{matrix[i, j, k]} {"("} {i}, {j}, {k} {")"} {" "}");
+            };
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-
-int[,,] Create3DMassive(int size1, int size2, int size3)
-{
-    int[,,] array = new int[size1, size2, size3];
-    int[] values = new int[countNums];
-    int num
-     = 10;
-    for (int i = 0; i < values.Length; i++)
-        values[i] = num
-        ++;
-
-    for (int i = 0; i < values.Length; i++)
-    {
-        int randomInd = new Random().Next(0, values.Length);
-        int temp = values[i];
-        values[i] = values[randomInd];
-        values[randomInd] = temp;
-    }
-
-    int valueIndex = 0;
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                array[i, j, k] = values[valueIndex++];
-            }
-        }
-    }
-    return array;
-}
-
-
-int InputInt(string output)
-{
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
-}
+int[,,] array = CreateMatrix(2, 2, 2);
+CreateArrayRnd(array);
+Console.WriteLine();
+PrintMatrix(array);
